eada332125e8acb721c086f29b0d295b87bdcec1
JIBIN JAIN JOHNY <jibinjjohny11@gmail.com>
2023-09-22 10:04:20 +0200

    Add allowed host

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['newblog-242242d7bf21.herokuapp.com', 'localhost']
 
 
 # Application definition
